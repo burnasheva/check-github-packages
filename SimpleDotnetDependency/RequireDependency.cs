c4eda39a7d7f6f454a8e6e777b73c78b573f3f5f
Nadia Burnasheva <nadezhda.burnasheva@jetbrains.com>
2023-03-11 10:41:27 +0100

    add simple project

---
+++ SimpleDotnetDependency/RequireDependency.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml;
+using Newtonsoft.Json;
+
+namespace SimpleDotnetDependency;
+
+public class RequireDependency
+{
+    public void checkDependency()
+    {
+        String simpleString = "Hello, world";
+        JsonConvert.SerializeObject(simpleString);
+    }
+}
